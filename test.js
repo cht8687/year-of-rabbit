b5cb221fe2ef026db009c5aa538b3b8e1a86ba57
cht8687 <cht8687@gmail.com>
2016-09-17 09:47:34 +1000

    init

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-var yearOfRat = require('./');
+var yearOfRabbit = require('./');
 var test = require('tape');
 
-test('it should check if a year is year of rat', function(t) {
+test('it should check if a year is year of rabbit', function(t) {
   t.plan(4);
-  t.equal(yearOfRat(1984), true, "1984 is year of rat");
-  t.equal(yearOfRat(1985), false, "1985 is not year of rat");
-  t.equal(yearOfRat(new Date(1996, 0)), true, "1996 is year of rat");
-  t.equal(typeof yearOfRat() === 'boolean', true);
+  t.equal(yearOfRabbit(1987), true, "1987is year of rabbit");
+  t.equal(yearOfRabbit(1988), false, "1988 is not year of rabbit");
+  t.equal(yearOfRabbit(new Date(1999, 0)), true, "1999 is year of rabbit");
+  t.equal(typeof yearOfRabbit() === 'boolean', true);
 });
